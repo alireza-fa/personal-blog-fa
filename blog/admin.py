c38e4a20bfbfbf5437825297bc6248c6ffaaa3bb
alireza-fa <alirezafeyze44@gmail.com>
2023-01-20 21:34:29 +0330

    completed admin.py(core and blog)

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post, Category, PostCategory, PostTag
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active')
+    list_filter = ('is_active',)
+    list_editable = ('is_active',)
+    search_fields = ('name', 'name_en')
+    prepopulated_fields = {"slug": ('name_en',)}
+
+
+class PostCategoryInline(admin.TabularInline):
+    model = PostCategory
+    extra = 1
+
+
+class PostTagInline(admin.TabularInline):
+    model = PostTag
+    extra = 3
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'is_active')
+    list_filter = ('is_active',)
+    list_editable = ('is_active',)
+    search_fields = ('author', 'title', 'title_en', 'body')
+    prepopulated_fields = {"slug": ('title_en',)}
+    inlines = (PostCategoryInline, PostTagInline)
--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import AboutMe, Contact, Advertisement, InstagramPost, Tag
+
+
+@admin.register(AboutMe)
+class AboutMeAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'position', 'address')
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('fullname', 'subject', 'is_read', 'is_active', 'created')
+    list_filter = ('is_active', 'is_read')
+    list_editable = ('is_active', 'is_read')
+    search_fields = ('fullname', 'email', 'subject', 'message')
+
+
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ('name', 'expired_at', 'image')
+    search_fields = ('name',)
+
+
+@admin.register(InstagramPost)
+class InstagramPostAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'image')
+    search_fields = ('name',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created')
+    search_fields = ('name',)
