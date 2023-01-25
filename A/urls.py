396e82416541fe76880564d0857ef794f66b0ae9
alireza-fa <alirezafeyze44@gmail.com>
2023-01-25 22:15:40 +0330

    personal blog fa v1

--- A/urls.py
+++ A/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('blog/', include('blog.urls')),
+    path('error404/', TemplateView.as_view(template_name='404.html')),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.db.models import Q
 from django.http import JsonResponse
-from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import get_object_or_404
 from django.template.loader import render_to_string
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
